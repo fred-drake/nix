238a9cbb7c3dbb1b6a8ebe5e6863ce601b3d4f75
fred-drake <fred.drake@gmail.com>
2024-09-02 18:17:18 -0400

    Improved VSCode extensions

--- dev-environments/rust/flake.nix
+++ dev-environments/rust/flake.nix
@@ -22,24 +22,33 @@
   outputs = { self, nixpkgs, nix-vscode-extensions, ... }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: let
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;  # Allow unfree packages
         };
+        commonVSCodeExtensions = import ../../apps/vscode-extensions.nix { inherit pkgs nix-vscode-extensions; };
+        marketplace = nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace;
+      in f {
+        inherit pkgs commonVSCodeExtensions marketplace;
       });
     in
     {
 
-      devShells = forEachSupportedSystem ({ pkgs }: {
+      devShells = forEachSupportedSystem ({ pkgs, commonVSCodeExtensions, marketplace }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
             cargo    # Rust's package manager and build tool
             rustc    # The Rust compiler
             rustfmt  # Rust code formatter
             clippy   # Rust linter for catching common mistakes and improving code style
+            libiconv
+            gcc
+            clang
+            llvmPackages.libclang
+            pkg-config
             (vscode-with-extensions.override {
-              vscodeExtensions = with nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
+              vscodeExtensions = commonVSCodeExtensions.common ++ (with marketplace; [
                 rust-lang.rust-analyzer        # Rust language server for code analysis and intelligent features
                 tamasfe.even-better-toml       # Enhanced TOML file support, useful for Cargo.toml files
                 vadimcn.vscode-lldb            # Native debugger support for Rust
@@ -47,7 +56,9 @@
                 usernamehw.errorlens           # Inline error and warning highlighting
                 bierner.docs-view              # Markdown documentation viewer, helpful for Rust docs
                 belfz.search-crates-io         # Quick search and integration with crates.io
-              ];
+              ]);
+              # vscodeExtensions = with nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
+              # ];
             })
           ];
 
@@ -59,6 +70,12 @@
           };
 
           shellHook = ''
+            export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
+            export LLVM_CONFIG_PATH="${pkgs.llvm}/bin/llvm-config"
+            export CPATH="${pkgs.darwin.apple_sdk.frameworks.Metal}/Library/Frameworks:${pkgs.darwin.apple_sdk.frameworks.Foundation}/Library/Frameworks:${pkgs.darwin.apple_sdk.frameworks.MetalKit}/Library/Frameworks"
+            export METAL_PATH="${pkgs.darwin.apple_sdk.frameworks.Metal}/Library/Frameworks"
+            export LIBRARY_PATH="$LIBRARY_PATH:$METAL_PATH"
+            export FRAMEWORK_SEARCH_PATHS="$FRAMEWORK_SEARCH_PATHS:$METAL_PATH"
             echo "Rust environment is ready"
             cargo version
           '';
