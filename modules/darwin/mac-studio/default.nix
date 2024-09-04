aef0bacdd1120e98f251aada00709c2440cb5347
fred-drake <fred.drake@gmail.com>
2024-09-04 10:40:26 -0400

    Added apps for mac studio

--- modules/darwin/mac-studio/default.nix
+++ modules/darwin/mac-studio/default.nix
@@ -1,4 +1,11 @@
 # Configuration specific to the Mac Studio machine
 { ... }: {
+  homebrew = {
+    casks = [ "mutedeck" "proxy-audio-device" ]; 
+    masApps = {
+      "iWallpaper - Live Wallpaper" = 1552826194;
+    };
+  };
+
   system.defaults.NSGlobalDomain."com.apple.trackpad.scaling" = 2.0; # Set trackpad speed to a faster setting
 }
