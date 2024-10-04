c0c4fb9901967af2e56c118aea093628b97e01cf
Fred Drake <fred.drake@gmail.com>
2024-10-03 20:23:56 -0400

    Moved to alejadra formatter with git precommit check

--- development/flake.nix
+++ development/flake.nix
@@ -4,28 +4,43 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = { allowUnfree = true; };
+  outputs = {self, ...} @ inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config = {allowUnfree = true;};
+      };
+    in {
+      checks = {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            alejandra.enable = true;
+            nixpkgs-fmt.enable = true;
+            just.enable = true;
+          };
         };
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
+      };
+      devShells.default = pkgs.mkShell {
+        buildInputs =
+          self.checks.${system}.pre-commit-check.enabledPackages
+          ++ (with pkgs; [
             git
             cargo
             just
             aider-chat
-            nixfmt-rfc-style # for formatting nix files
-          ];
+            alejandra
+          ]);
 
-          shellHook = ''
-            echo "Welcome to this Nix flake development environment!"
-          '';
-        };
-      });
+        shellHook = ''
+          echo "Welcome to this Nix flake development environment!"
+        '';
+      };
+    });
 }
