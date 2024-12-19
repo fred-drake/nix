215e680278e98970c1845543b4e65c005c82ebba
Fred Drake <fred.drake@gmail.com>
2024-12-18 23:44:44 -0500

    Updated revs

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -25,14 +25,10 @@
   };
 
   # Font configuration
-  fonts.packages = [
-    (pkgs.nerdfonts.override {
-      fonts = [
-        "Hack"
-        "JetBrainsMono"
-        "Meslo"
-      ];
-    })
+  fonts.packages = with pkgs; [
+    nerd-fonts.hack
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.meslo-lg
   ];
 
   # Homebrew configuration
