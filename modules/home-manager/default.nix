edc845719c46f804e596f812b627a0f3e61b2433
Fred Drake <fred.drake@gmail.com>
2025-01-21 10:41:43 -0500

    Added syncthing CLI

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -96,6 +96,7 @@
     slack # Team communication tool
     sops # Secret management tool
     spotify # Music streaming service
+    stc-cli # Syncthing CLI
     syncthing # File synchronization tool
     tldr # Documentation tool
     tmux # Terminal multiplexer
