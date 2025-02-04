a2ae3d27172d05a5257f07ec379de7d414a10070
Fred Drake <fred.drake@gmail.com>
2025-02-04 16:38:55 -0500

    Wheel doesn't need password with nixoswinnvim

--- modules/nixos/nixoswinvm/configuration.nix
+++ modules/nixos/nixoswinvm/configuration.nix
@@ -14,10 +14,10 @@
 
   # RECOMMENDED
   # - use 800x600 resolution for text console, to make it easy to fit on screen
-  boot.kernelParams = ["video=hyperv_fb:800x600"];  # https://askubuntu.com/a/399960
+  boot.kernelParams = ["video=hyperv_fb:800x600"]; # https://askubuntu.com/a/399960
   # - avoid a problem with `nix-env -i` running out of memory
   boot.kernel.sysctl."vm.overcommit_memory" = "1"; # https://github.com/NixOS/nix/issues/421
-  
+
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
     # devices = [ ];
@@ -37,6 +37,9 @@
     # dhcpcd.enable = false;
     # interfaces.ens18.
   };
+
+  security.sudo.wheelNeedsPassword = false;
+
   users.users.fdrake = {
     isNormalUser = true;
     home = "/home/fdrake";
