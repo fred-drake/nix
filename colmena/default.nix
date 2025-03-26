d1d2a49b0c37f3d44e5ed015b6011c9313d6cc59
Fred Drake <fred.drake@gmail.com>
2025-03-26 15:13:48 -0400

    Fixing colmena configuration

--- colmena/default.nix
+++ colmena/default.nix
@@ -7,16 +7,33 @@
   ...
 }: let
   soft-secrets = import "${secrets}/soft-secrets";
+
+  # Import host configurations
+  adguard1 = import ./hosts/adguard1.nix {inherit self nixpkgs-stable nixos-hardware secrets sops-nix;};
+  adguard2 = import ./hosts/adguard2.nix {inherit self nixpkgs-stable nixos-hardware secrets sops-nix;};
+  overseerr = import ./hosts/overseerr.nix {inherit self nixpkgs-stable secrets sops-nix;};
+  sonarr = import ./hosts/sonarr.nix {inherit self nixpkgs-stable secrets sops-nix;};
 in {
   meta = {
     nixpkgs = import nixpkgs-stable {system = "aarch64-linux";};
   };
 
-  # Import all the individual host configurations
-  imports = [
-    ./hosts/adguard1.nix
-    ./hosts/adguard2.nix
-    ./hosts/overseerr.nix
-    ./hosts/sonarr.nix
-  ];
+  # Merge all host configurations
+  # Base configurations
+  _adguard1 = adguard1._adguard1;
+  _adguard2 = adguard2._adguard2;
+  _overseerr = overseerr._overseerr;
+  _sonarr = sonarr._sonarr;
+
+  # Init configurations
+  "adguard1-init" = adguard1."adguard1-init";
+  "adguard2-init" = adguard2."adguard2-init";
+  "overseerr-init" = overseerr."overseerr-init";
+  "sonarr-init" = sonarr."sonarr-init";
+
+  # Full configurations
+  "adguard1" = adguard1."adguard1";
+  "adguard2" = adguard2."adguard2";
+  "overseerr" = overseerr."overseerr";
+  "sonarr" = sonarr."sonarr";
 }
