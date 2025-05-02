40add22e1285f7ad88b983fb49d23c6ae8955e8d
Fred Drake <fred.drake@gmail.com>
2025-05-02 12:39:40 -0400

    Fixed docker socket on gitea-runner-1

--- apps/gitea-runner-1.nix
+++ apps/gitea-runner-1.nix
@@ -2,23 +2,27 @@
   virtualisation.containers.enable = true;
   virtualisation.podman = {
     enable = true;
+    dockerSocket.enable = true;
     dockerCompat = true;
     defaultNetwork.settings.dns_enabled = true;
   };
   virtualisation.oci-containers = {
     backend = "podman";
     containers = {
-      gitea = {
+      gitea-runner-1 = {
         image = "docker.gitea.com/act_runner:latest";
         autoStart = true;
+        volumes = [
+          "${config.sops.secrets.gitea-registration-token.path}:/gitea-registration-token"
+          "/var/run/docker.sock:/var/run/docker.sock:ro"
+        ];
         environment = {
           PUID = "1000";
           PGID = "1000";
           TZ = "America/New_York";
           GITEA_INSTANCE_URL = "https://gitea.${config.soft-secrets.networking.domain}";
-          GITEA_RUNNER_REGISTRATION_TOKEN_FILE = config.sops.secrets.gitea-registration-token.path;
+          GITEA_RUNNER_REGISTRATION_TOKEN_FILE = "/gitea-registration-token";
           GITEA_RUNNER_NAME = "gitea-runner-1";
-          DOCKER_HOST = "unix:///run/user/1000/podman/podman.sock";
         };
       };
     };
