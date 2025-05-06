6de2bc9032d6159677f4f810015fb7173683bc13
Fred Drake <fred.drake@gmail.com>
2025-05-06 19:57:08 -0400

    Added proxmox provisioning script

---
+++ proxmox-provision-nixos.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+PROXMOX_SERVER=thrall
+CONTAINER_ID=109
+HOSTNAME=my-host-name
+STORAGE=local-lvm
+MEMORY=4096
+DISK_SIZE_IN_GB=100
+UNPRIVILEGED=1
+
+ssh $PROXMOX_SERVER "pct create $CONTAINER_ID \
+    --arch amd64 local:vztmpl/nixos-system-x86_64-linux.tar.xz \
+    --ostype unmanaged \
+    --description nixos \
+    --hostname $HOSTNAME \
+    --net0 name=eth0,bridge=vmbr3,ip=dhcp,firewall=1 \
+    --storage $STORAGE \
+    --memory 4096 \
+    --rootfs $STORAGE:$DISK_SIZE_IN_GB \
+    --unprivileged $UNPRIVILEGED \
+    --features nesting=1 \
+    --cmode console \
+    --onboot 1 \
+    --start 1"
