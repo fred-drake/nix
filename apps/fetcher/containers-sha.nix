77df7bc65e4ec40733b64c20e0ef73b72743d4c1
Fred Drake <fred.drake@gmail.com>
2025-05-07 14:43:03 -0400

    Updated containers

--- apps/fetcher/containers-sha.nix
+++ apps/fetcher/containers-sha.nix
@@ -2,22 +2,29 @@
 # Auto-generated -- do not modify! #
 ####################################
 {pkgs, ...}: {
-  "docker.gitea.com" = {
-    "gitea" = {
-      "1-rootless" = {
-        "linux/amd64" = "docker.gitea.com/gitea@sha256:5436192de9b42c7a1b3f456a3b19d58855a5c7959077258901f2f6a3b593af95";
+  "docker.io" = {
+    "louislam/uptime-kuma" = {
+      "1" = {
+        "linux/amd64" = "docker.io/louislam/uptime-kuma@sha256:b4c3a4d186b4612b3a7bbb39c56a634626276615c21871c837a86e4a43d8e047";
       };
     };
+  };
+  "docker.gitea.com" = {
     "act_runner" = {
       "latest" = {
         "linux/amd64" = "docker.gitea.com/act_runner@sha256:eabea0883261fa3378849a1b9ce86dce6a2e906c8db48ad8553edc3f2bd9ff68";
+      };
+    };
+    "gitea" = {
+      "1-rootless" = {
+        "linux/amd64" = "docker.gitea.com/gitea@sha256:5436192de9b42c7a1b3f456a3b19d58855a5c7959077258901f2f6a3b593af95";
       };
     };
   };
   "docker.n8n.io" = {
     "n8nio/n8n" = {
       "latest" = {
-        "linux/amd64" = "docker.n8n.io/n8nio/n8n@sha256:2097bb4bdf196ed8c0f0562aa1c2837d66f3ed64e28e0d4cb3be4b7e202df01b";
+        "linux/amd64" = "docker.n8n.io/n8nio/n8n@sha256:d153a229a40d4e1c25dc85d40688ea9506cffe7abf20ca135d45a9f290790e54";
       };
     };
   };
