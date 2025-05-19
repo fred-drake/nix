e6c962e8ccf090ea8f8681c32fc3bfbd385e3ce5
Fred Drake <fred.drake@gmail.com>
2025-05-18 20:25:22 -0400

    refactor: remove sonarr, radarr, and sabnzbd configurations and hosts

--- colmena/default.nix
+++ colmena/default.nix
@@ -10,10 +10,7 @@
   adguard1 = import ./hosts/adguard1.nix {inherit self nixpkgs-stable nixos-hardware secrets sops-nix;};
   adguard2 = import ./hosts/adguard2.nix {inherit self nixpkgs-stable nixos-hardware secrets sops-nix;};
   overseerr = import ./hosts/overseerr.nix {inherit self nixpkgs-stable secrets sops-nix;};
-  sonarr = import ./hosts/sonarr.nix {inherit self nixpkgs-stable secrets sops-nix;};
-  radarr = import ./hosts/radarr.nix {inherit self nixpkgs-stable secrets sops-nix;};
   prowlarr = import ./hosts/prowlarr.nix {inherit self nixpkgs-stable secrets sops-nix;};
-  sabnzbd = import ./hosts/sabnzbd.nix {inherit self nixpkgs-stable secrets sops-nix;};
   n8n = import ./hosts/n8n.nix {inherit self nixpkgs-stable secrets sops-nix;};
   gitea = import ./hosts/gitea.nix {inherit self nixpkgs-stable secrets sops-nix;};
   gitea-runner-1 = import ./hosts/gitea-runner-1.nix {inherit self nixpkgs-stable secrets sops-nix;};
@@ -33,10 +30,7 @@ in {
   _adguard1 = adguard1._adguard1;
   _adguard2 = adguard2._adguard2;
   _overseerr = overseerr._overseerr;
-  _sonarr = sonarr._sonarr;
-  _radarr = radarr._radarr;
   _prowlarr = prowlarr._prowlarr;
-  _sabnzbd = sabnzbd._sabnzbd;
   _n8n = n8n._n8n;
   _gitea = gitea._gitea;
   _gitea-runner-1 = gitea-runner-1._gitea-runner-1;
@@ -51,10 +45,7 @@ in {
   "adguard1-init" = adguard1."adguard1-init";
   "adguard2-init" = adguard2."adguard2-init";
   "overseerr-init" = overseerr."overseerr-init";
-  "sonarr-init" = sonarr."sonarr-init";
-  "radarr-init" = radarr."radarr-init";
   "prowlarr-init" = prowlarr."prowlarr-init";
-  "sabnzbd-init" = sabnzbd."sabnzbd-init";
   "n8n-init" = n8n."n8n-init";
   "gitea-init" = gitea."gitea-init";
   "gitea-runner-1-init" = gitea-runner-1."gitea-runner-1-init";
@@ -69,10 +60,7 @@ in {
   "adguard1" = adguard1."adguard1";
   "adguard2" = adguard2."adguard2";
   "overseerr" = overseerr."overseerr";
-  "sonarr" = sonarr."sonarr";
-  "radarr" = radarr."radarr";
   "prowlarr" = prowlarr."prowlarr";
-  "sabnzbd" = sabnzbd."sabnzbd";
   "n8n" = n8n."n8n";
   "gitea" = gitea."gitea";
   "gitea-runner-1" = gitea-runner-1."gitea-runner-1";
