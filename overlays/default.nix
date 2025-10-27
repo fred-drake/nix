901399359e7e4c90f8d142aacbed80965129cfb4
Fred Drake <fred.drake@gmail.com>
2025-10-26 23:37:43 -0400

    chore(overlay): update Spotify ARM64 package hash

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,7 +4,7 @@
   spotify = prev.spotify.overrideAttrs (oldAttrs: {
     src = prev.fetchurl {
       url = oldAttrs.src.url or "https://download.scdn.co/SpotifyARM64.dmg";
-      sha256 = "sha256-K+dwlT4hd/SWbQT23ESZY8gGQ8bf5x5CpepMz5Wd6Ng=";
+      sha256 = "sha256-0gwoptqLBJBM0qJQ+dGAZdCD6WXzDJEs0BfOxz7f2nQ=";
     };
   });
 }
