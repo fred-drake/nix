243dd54089a105a01b210241e3ec2c3e43dd9a19
Fred Drake <fred.drake@gmail.com>
2026-03-25 00:03:05 -0400

    feat(fredpc): add extra binary cache substituters for cuda, hyprland, nix-community, nix-gaming, and nixpkgs-wayland

--- modules/nixos/host/fredpc/configuration.nix
+++ modules/nixos/host/fredpc/configuration.nix
@@ -153,7 +153,23 @@ in {
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
-    settings.trusted-users = ["root" "fdrake"];
+    settings = {
+      trusted-users = ["root" "fdrake"];
+      extra-substituters = [
+        "https://cuda-maintainers.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://nix-gaming.cachix.org"
+        "https://nixpkgs-wayland.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      ];
+    };
   };
 
   programs = {
